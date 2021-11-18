1dadb2bab7d664dc9a34e90839f3e0b8f5f446f3
thisoban <thisoban_1995@hotmail.com>
2021-11-18 15:27:43 +0100

    edited testingigdbclass

--- BackEnd/GameLibraryBackEnd/DAL/testingigdbclass.cs
+++ BackEnd/GameLibraryBackEnd/DAL/testingigdbclass.cs
@@ -8,19 +8,19 @@ namespace DAL
 {
     class testingigdbclass
     {
-        public async Task<Game> getgame()
-        {
-            var igdb = new IGDBClient(
-                // Found in Twitch Developer portal for your app
-                Environment.GetEnvironmentVariable("dtyf1m6sh84pvfxpml37o5nv9ybapy"),
-                Environment.GetEnvironmentVariable("veeb1lzntrosmmk0fzg93y90cn12xi")
-            );
+        //public async Task<Game> getgame()
+        //{
+        //    var igdb = new IGDBClient(
+        //        // Found in Twitch Developer portal for your app
+        //        Environment.GetEnvironmentVariable("dtyf1m6sh84pvfxpml37o5nv9ybapy"),
+        //        Environment.GetEnvironmentVariable("veeb1lzntrosmmk0fzg93y90cn12xi")
+        //    );
 
-            var games = await igdb.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: "fields id,name; where id = 4;");
-            var game = games.First();
+        //    var games = await igdb.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: "fields id,name; where id = 4;");
+        //    var game = games.First();
 
-            Console.WriteLine(game.Name, game.Id);
-            return game;
-        }
+        //    Console.WriteLine(game.Name, game.Id);
+        //    return game;
+        //}
     }
 }

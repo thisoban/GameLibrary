a50acb2893b973d65d2175fb6603bc19a2268b31
thisoban <thisoban_1995@hotmail.com>
2021-12-02 08:33:15 +0100

    tweaking code and adding features

---
+++ FrontEnd/game-library-frontend/src/Pages/GamesPage.js
@@ -0,0 +1,21 @@
+
+
+import GameCard from '../Components/GameCard'
+import React from 'react'   
+// import { buttonBaseClasses } from '@mui/material';
+// import { display } from '@mui/system';
+
+
+function GamesPage() {
+
+    return (
+       
+        <div>
+          
+            <p>Games page</p>
+        </div>
+                     
+    )
+}
+
+export default GamesPage

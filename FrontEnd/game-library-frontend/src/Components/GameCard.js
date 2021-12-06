59660c3f9074dc8eb521341e22e07b451162bcf4
thisoban <thisoban_1995@hotmail.com>
2021-12-06 20:35:23 +0100

    tweaking code

--- FrontEnd/game-library-frontend/src/Components/GameCard.js
+++ FrontEnd/game-library-frontend/src/Components/GameCard.js
@@ -5,33 +5,44 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { cardActionAreaClasses } from '@mui/material';
 
 
+function Cardlist({Cards}){
 
+}
 
-function GameCard(){
+function GameCard(){ 
     return (
-        
-        <Card sx={{ maxWidth: 300 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
+        <div className = "game">
+          {Cards.map((Card) =>
+          <div key= {Card.id}>
+            <Card sx={{ maxWidth: 300 }}>
+           <CardMedia
+             component="img"
+             height="140"
+             image="/static/images/cards/contemplative-reptile.jpg"
+             alt="green iguana"
+           />
+           <CardContent>
+             <Typography gutterBottom variant="h5" component="div">
+               Lizard
+             </Typography>
+             <Typography variant="body2" color="text.secondary">
+               Lizards are a widespread group of squamate reptiles, with over 6,000
+               species, ranging across all continents except Antarctica
+             </Typography>
+           </CardContent>
+           <CardActions>
+             <Button size="small">Learn More</Button>
+           </CardActions>
+         </Card>
+             </div>
+           
+          )}
+               
+        </div>
+       
       );
 }
 
